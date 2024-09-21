012177dbb87ec6f0898f70224effe4bd4aa9c363
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2024-09-21 15:47:08 +0700

    update url resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -9,5 +9,5 @@ export const personalData = {
   github: 'https://github.com/truonggiang2512',
   facebook: 'https://www.facebook.com/truonggiang.hanguyen.5',
   linkedIn: 'https://www.linkedin.com/in/gianghnt/',
-  resume: "https://drive.google.com/file/d/1eN55UJXUvNaeybT1AdV8R04X3LGkokGL/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1TM8HuAl_ykGWmMgtm_DYcUyETVreRXKD/view?usp=sharing"
 }
