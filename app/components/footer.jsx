697070a89db807adc06a20f5c5eae487813c2405
truonggiang2512 <hanguyentruonggiang004@gmail.com>
2024-09-21 15:58:51 +0700

    update ui , link url

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
             © Developer Portfolio by{" "}
             <Link
               target="_blank"
-              href="https://www.linkedin.com/in/abu-said-bd/"
+              href="https://www.linkedin.com/in/gianghnt/"
               className="text-[#16f2b3]"
             >
               Hà Nguyễn Trường Giang
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -58,16 +58,6 @@ function Navbar() {
           <li>
             <Link
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
-              href="/blog"
-            >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
-                BLOGS
-              </div>
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="block px-4 py-2 no-underline outline-none hover:no-underline"
               href="/#projects"
             >
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
